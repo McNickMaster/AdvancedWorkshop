eaea26b855e213c7a122a570872e6088933bf054
Downeyy <90855407+Downeyy@users.noreply.github.com>
2023-09-24 21:35:44 -0400

    Added In The Moving Platform

---
+++ AdvancedWorkshop/Assets/Scripts/Level/PlatformMoving.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float speed = 5f;         // Speed at which the platform moves.
+    public float leftBoundary = -5f; // Left boundary of movement.
+    public float rightBoundary = 5f; // Right boundary of movement.
+
+    private bool movingRight = true; // Flag to track movement direction.
+
+    private void Update()
+    {
+        // Calculate the platform's new position.
+        Vector3 newPosition = transform.position;
+
+        // Update the position based on the movement direction and speed.
+        if (movingRight)
+        {
+            newPosition.x += speed * Time.deltaTime;
+            if (newPosition.x > rightBoundary)
+            {
+                newPosition.x = rightBoundary;
+                movingRight = false;
+            }
+        }
+        else
+        {
+            newPosition.x -= speed * Time.deltaTime;
+            if (newPosition.x < leftBoundary)
+            {
+                newPosition.x = leftBoundary;
+                movingRight = true;
+            }
+        }
+
+        // Apply the new position to the platform.
+        transform.position = newPosition;
+    }
+}
