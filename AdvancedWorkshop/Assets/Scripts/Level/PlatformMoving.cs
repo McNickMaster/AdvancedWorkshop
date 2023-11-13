832f0ec9faf127fc37542aa90146acb44e3ebefd
Downeyy <90855407+Downeyy@users.noreply.github.com>
2023-11-13 17:23:32 -0500

    Updated Platforms

--- AdvancedWorkshop/Assets/Scripts/Level/PlatformMoving.cs
+++ AdvancedWorkshop/Assets/Scripts/Level/PlatformMoving.cs
@@ -2,59 +2,53 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    public float speed = 5f;         // Speed at which the platform moves.
-    public float leftBoundary = -5f; // Left boundary of movement.
-    public float rightBoundary = 5f; // Right boundary of movement.
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed;
+    [SerializeField] private float checkDistance = 0.05f;
 
-    private bool movingRight = true; // Flag to track movement direction.
 
-    private void Update()
+    private Transform targetWaypoint;
+    private int currentWaypoitIndex = 0;
+
+    void Start()
     {
-        // Calculate the platform's new position.
-        Vector3 newPosition = transform.position;
-
-        // Update the position based on the movement direction and speed.
-        if (movingRight)
-        {
-            newPosition.x += speed * Time.deltaTime;
-            if (newPosition.x > rightBoundary)
-            {
-                newPosition.x = rightBoundary;
-                movingRight = false;
-            }
-        }
-        else
-        {
-            newPosition.x -= speed * Time.deltaTime;
-            if (newPosition.x < leftBoundary)
-            {
-                newPosition.x = leftBoundary;
-                movingRight = true;
-            }
-        }
-
-        // Apply the new position to the platform.
-        transform.position = newPosition;
+        targetWaypoint = waypoints[0];
     }
 
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, speed * Time.deltaTime);
+
+
+        if (Vector2.Distance(transform.position, targetWaypoint.position) < checkDistance)
+        {
+            targetWaypoint = GetNextWaypoint();
+        }
+    }
+
+    private Transform GetNextWaypoint()
+    {
+        currentWaypoitIndex++;
+        if (currentWaypoitIndex >= waypoints.Length)
+        {
+            currentWaypoitIndex = 0;
+        }
+        return waypoints[currentWaypoitIndex];
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-if (other.tag=="Player")
-            {
-            other.transform.parent.SetParent(transform);
-            Debug.Log("moving platform");
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform;
+            Debug.Log("Player on the vertical moving platform.");
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag=="Player")
+        if (other.tag == "Player")
         {
-            other.transform.parent.SetParent(null);
-            Debug.Log("Player exited platform");
+            other.transform.parent = null;
         }
-    }
-    private void FixedUpdate()
-    {
-        
     }
 }
